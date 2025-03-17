01b1403cee1cdc005fad7b3b4fe4724c55b3b46b
Ayazhan <baltabaevaayazhan0@gmail.com>
2025-03-17 22:32:58 +0500

    price filter in bundles

--- apps/shops/bundles/filters.py
+++ apps/shops/bundles/filters.py
@@ -5,8 +5,19 @@ from apps.shops.bundles.models import Bundle
 
 class BundleFilterSet(filters.FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    from_price = filters.NumberFilter(method='filter_by_price_range')
+    to_price = filters.NumberFilter(method='filter_by_price_range')
 
     class Meta:
         model = Bundle
         fields = {
         }
+
+    def filter_by_price_range(self, queryset, name, value):
+        if name == 'from_price':
+            queryset = queryset.filter(price__gte=value)
+
+        if name == 'to_price':
+            queryset = queryset.filter(price__lte=value)
+
+        return queryset
